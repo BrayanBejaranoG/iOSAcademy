edff9b702f30a484a24588d0812447d8a16eb490
BrayanBejaranoG <92119106+BrayanBejaranoG@users.noreply.github.com>
2021-11-04 13:31:34 -0500

    Add files via upload

---
+++ Constraints/Exe_1/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Brayan Guillermo Bejarano Guerrero on 10/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
